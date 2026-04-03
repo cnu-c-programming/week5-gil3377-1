b12007e3dc2365a994773158459efa2a44d27842
gil3377 <mungilj3@gmail.com>
2026-04-03 10:10:02 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -2,12 +2,12 @@
 
 void read_only(const int *arr, int size) {
 
+    // arr[0] = 999; 
 
     for (int i = 0; i < size; i++) {
         printf("%d ", *(arr + i));
         printf("\n");
     }
-    
 }
 int main() {
     int nums[] = {10, 20, 30, 40, 50};
